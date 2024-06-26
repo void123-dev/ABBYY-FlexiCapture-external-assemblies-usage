61715b6a2a9f0d419598a0dea00e69239479243c
Aleksey Serov <killingspree_pro@hotmail.com>
2024-06-26 06:22:22 -0700

    Add project files.

---
+++ BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AbbyyDataExport
+{
+    public class BaseClass
+    {
+        public void Show(string text)
+        {
+            // will show text by standard FlexiCapture means static method void ShowMessage(string text, bool IsError) from static class FCTools ABBYY.FlexiCapture.FCTools.ShowMessage(text, false);
+            MessageBox.Show("Hello World");
+        }
+    }
+}

312f4e0dab40bbcb2e34fada13e48fb0610e49c4
Aleksey Serov <killingspree_pro@hotmail.com>
2024-06-26 07:05:40 -0700

    INitial base version

--- BaseClass.cs
+++ BaseClass.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿extern alias aControllerInterlop; // WARNING : delete
+using aControllerInterlop::ABBYY.FlexiCapture;
+
 using System.Windows;
 
 namespace AbbyyDataExport
 {
     public class BaseClass
     {
-        public void Show(string text)
+        public void ShowMessage(string text)
         {
             // will show text by standard FlexiCapture means static method void ShowMessage(string text, bool IsError) from static class FCTools ABBYY.FlexiCapture.FCTools.ShowMessage(text, false);
-            MessageBox.Show("Hello World");
+            //MessageBox.Show("Hello World");
+
+            FCTools.ShowMessage(text, false);
         }
     }
 }
